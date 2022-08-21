d68d289b16ef89a89845a41435ea1bb5a1e55bc7
Gayathry Suresh <62144916+Gayathry17@users.noreply.github.com>
2022-08-21 21:36:24 +0530

    card

--- src/components/SortEvents/Events.js
+++ src/components/SortEvents/Events.js
@@ -2,7 +2,12 @@ import React from "react";
 import Card from "../Card/Card";
 import "./Events.css";
 const Events = () => {
-  return <div>Sort Events</div>;
+  return <div className="events">
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+  </div>;
 };
 
 export default Events;
