72c6251a46e34b5af6f0f04ffca1438b40ffc695
Deepu19g <deepakmathew7d@gmail.com>
2022-08-22 11:59:18 +0530

    search feature added

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,60 +1,61 @@
-import React from 'react';
+import React from "react";
 
-import './Card.css';
+import "./Card.css";
 
-
-import { FaRegCalendarAlt } from 'react-icons/fa';
+import { FaRegCalendarAlt } from "react-icons/fa";
 import { IoIosTimer } from "react-icons/io";
-import { MdGroups } from 'react-icons/md';
-import { BiCoinStack } from 'react-icons/bi'
+import { MdGroups } from "react-icons/md";
+import { BiCoinStack } from "react-icons/bi";
 
-
-function Card() {
+function Card({ name, description, tags,teamSize,startDate,deadline,fees,endDate,length,img}) {
   return (
-    <div className='card__outer'>
-      <div className='event__card'>
-       <div className='eventcard__image'>
-        <img src="https://cdn.pixabay.com/photo/2015/04/19/08/32/marguerite-729510__340.jpg" alt=" " />
-       </div>
-       <div className='eventcard__desc'>
-        <div className='eventcard__title'>
-          <h2 className='desc__title'>Embrace</h2> 
-          <div className='desc__badge'>
-            <p>Ended</p>
+    <div className="card__outer">
+      <div className="event__card">
+        <div className="eventcard__image">
+          <img
+            src={img}
+            alt=" "
+          />
+        </div>
+        <div className="eventcard__desc">
+          <div className="eventcard__title">
+            <h2 className="desc__title">{name}</h2>
+            <div className="desc__badge">
+              <p>Ended</p>
+            </div>
+          </div>
+          <p className="eventcard__para">{description}</p>
+          <div className="eventcard__tags">
+            {tags.map((itm) => {
+              return (
+                <div className="event__tag">
+                  <p>{itm}</p>
+                </div>
+              );
+            })}
           </div>
         </div>
-        <p className='eventcard__para'>Embrace is a fundraising initiative organized by Excel 2021,in associative with CRY to ensure the welfare of the children protected under CRY and their service</p>
-        <div className='eventcard__tags'>
-          <div className='event__tag'>
-            <p>Social initiative</p>
+        <div className="eventcard__details">
+          <div className="event__detail">
+            <FaRegCalendarAlt className="detail__icon" />
+            <p>{startDate}-{endDate}</p>
           </div>
-          <div className='event__tag'>
-            <p>Fundraiser</p>
+          <div className="event__detail">
+            <IoIosTimer className="detail__icon" />
+            <p>{length}</p>
+          </div>
+          <div className="event__detail">
+            <MdGroups className="detail__icon" />
+            <p>{teamSize}</p>
+          </div>
+          <div className="event__detail">
+            <BiCoinStack className="detail__icon" />
+            <p>{fees}</p>
           </div>
         </div>
-       </div>
-       <div className='eventcard__details'>
-        <div className='event__detail'>
-           <FaRegCalendarAlt className='detail__icon' />
-           <p>04/12/2021-31/12/2021</p>
-        </div>
-        <div className='event__detail'>
-           <IoIosTimer className='detail__icon' />
-           <p>long</p>
-        </div>
-        <div className='event__detail'>
-           <MdGroups className='detail__icon' />
-           <p>Individual</p>
-        </div>
-        <div className='event__detail'>
-           <BiCoinStack className='detail__icon' />
-           <p>nil in prices</p>
-        </div>
-       </div>
+      </div>
     </div>
-    </div>
-    
-  )
+  );
 }
 
-export default Card
+export default Card;
--- src/components/SortEvents/Events.js
+++ src/components/SortEvents/Events.js
@@ -1,13 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "../Card/Card";
 import "./Events.css";
+import Data from "../../data/events.json"
 const Events = () => {
-  return <div className="events">
-    <Card />
-    <Card />
-    <Card />
-    <Card />
-  </div>;
+  const [searchtxt,setsearchtxt] = useState("");
+  const [data,setdata] = useState(Data)
+  let handlesearch = (e) => {
+    console.log(e.target.value);
+    setsearchtxt(e.target.value);
+    if (e.target.value !== "") {
+      const modified = Data.filter(
+        (itm) =>
+          itm.name && itm.name.toLowerCase().includes(e.target.value.toLowerCase())
+      );
+      console.log(modified)
+      setdata(modified);
+    } else {
+      setdata(Data);
+    }
+  };
+  
+  return (
+    <div className="events">
+      <input type="text" onChange={handlesearch} value={searchtxt}></input>
+{
+  data.map((itm)=>{
+    return(
+     <Card {...itm}/>
+    )
+  })
+}
+      
+      
+    </div>
+  );
 };
 
 export default Events;
