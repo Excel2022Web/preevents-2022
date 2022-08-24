432421431423d8ed001dc1e83f8368d921249a0a
Deepu19g <deepakmathew7d@gmail.com>
2022-08-24 23:33:18 +0530

    added searchbar

--- src/components/SortEvents/Events.js
+++ src/components/SortEvents/Events.js
@@ -1,37 +1,53 @@
 import React, { useState } from "react";
 import Card from "../Card/Card";
 import "./Events.css";
-import Data from "../../data/events.json"
+import Data from "../../data/events.json";
+
+import { BsSearch } from "react-icons/bs";
 const Events = () => {
-  const [searchtxt,setsearchtxt] = useState("");
-  const [data,setdata] = useState(Data)
+  const [searchtxt, setsearchtxt] = useState("");
+  const [data, setdata] = useState(Data);
+
   let handlesearch = (e) => {
     console.log(e.target.value);
     setsearchtxt(e.target.value);
     if (e.target.value !== "") {
       const modified = Data.filter(
         (itm) =>
-          itm.name && itm.name.toLowerCase().includes(e.target.value.toLowerCase())
+          itm.name &&
+          itm.name.toLowerCase().includes(e.target.value.toLowerCase())
       );
-      console.log(modified)
+      // console.log(modified)
       setdata(modified);
     } else {
       setdata(Data);
     }
   };
-  
+
   return (
     <div className="events">
-      <input type="text" onChange={handlesearch} value={searchtxt}></input>
-{
-  data.map((itm)=>{
-    return(
-     <Card {...itm}/>
-    )
-  })
-}
-      
-      
+      <div style={{ display: "flex", alignItems: "baseline" }}>
+        <div className="Events-searchbox">
+         
+          <input
+            type="text"
+            className="Events-serchbar"
+            onChange={handlesearch}
+            value={searchtxt}
+            placeholder="Search"
+          ></input>
+          <BsSearch></BsSearch>
+        </div>
+
+        <p className="Events-headerp">Upcoming</p>
+        <p className="Events-headerp">Ongoing</p>
+        <p className="Events-headerp">Ended</p>
+        <p className="Events-headerp">Filter</p>
+      </div>
+
+      {data.map((itm) => {
+        return <Card {...itm} />;
+      })}
     </div>
   );
 };
